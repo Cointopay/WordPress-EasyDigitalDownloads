4d83195e98a9a069af78fed06c25c31299ad2d30
Goshila <41993177+Goshila@users.noreply.github.com>
2022-05-23 15:42:17 +0500

    version updated

--- easy-digital-downloads-cointopay-gateway.php
+++ easy-digital-downloads-cointopay-gateway.php
@@ -3,7 +3,7 @@
  * Plugin Name: Cointopay Gateway for Easy Digital Downloads
  * Description: Cointopay payment gateway for Easy Digital Downloads
  * Author: Cointopay.com
- * Version: 1.5
+ * Version: 1.6
  * Author URI: https://cointopay.com/
  * Plugin URI: https://cointopay.com/
  *
